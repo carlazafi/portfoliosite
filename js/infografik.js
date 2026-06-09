c0288682b9c52a083bd0b3016d1ab336471fa5bf
Carla Zafirakos <caza1000@stud.ek.dk>
2026-06-09 22:04:01 +0200

    rettelse af info box i om mig

--- js/infografik.js
+++ js/infografik.js
@@ -9,12 +9,16 @@ document.addEventListener("click", (e) => {
 
 function infoWork() {
   console.log("infoWork");
+  const box = document.querySelector(".info-text");
+  box.classList.add("visible");
   document.querySelector(".info-text > h2").textContent = "Arbejde";
   document.querySelector(".placeholder").innerHTML = "<p>De seneste år har jeg beskæftiget mig med værtsskab i mange afarter. <br> Min baggrund rummer alt fra lærergerningen til lederskab, dyk ned i mit CV herunder</p>";
 }
 
 function infoHobby() {
   console.log("infoHobby");
+  const box = document.querySelector(".info-text");
+  box.classList.add("visible");
   document.querySelector(".info-text > h2").textContent = "Hobby";
   document.querySelector(".placeholder").innerHTML =
     "<p>Jeg driver podcasten Queer Me Up som vært og skaber. <br> <a href='https://linktr.ee/queer.me.up?utm_source=linktree_profile_share&ltsid=7a1220ae-16e7-4cde-9fc3-82fefd6a8c2d' target='_blank' rel='noopener'>Lyt her</a></p>";
@@ -22,6 +26,8 @@ function infoHobby() {
 
 function infoFunfact() {
   console.log("infoFunfact");
+  const box = document.querySelector(".info-text");
+  box.classList.add("visible");
   document.querySelector(".info-text > h2").textContent = "Fun fact";
   document.querySelector(".placeholder").innerHTML = "Jeg er hjertebarn og har et anatomisk hjerte tatoveret på venstre arm -tæt på hjertet";
 }
@@ -29,4 +35,5 @@ function infoFunfact() {
 function clearInfo() {
   document.querySelector(".info-text > h2").textContent = "";
   document.querySelector(".placeholder").innerHTML = "";
+  document.querySelector(".info-text").classList.remove("visible");
 }
